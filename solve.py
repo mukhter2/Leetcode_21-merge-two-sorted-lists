7a3d735be8a057419bf921469ca852f4d82f9b1f
Mukhter Hossain <mukhter.hossain@g.bracu.ac.bd>
2022-06-05 15:13:16 +0600

    Update solve.py

--- solve.py
+++ solve.py
@@ -6,16 +6,21 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         lst=[]
-        while(list1.next != None):
+        
+        while(list1 and list1.next is not None):
             lst.append(list1.val)
             list1=list1.next
-        lst.append(list1.val)
-        list1=list1.next
-        while(list2.next != None):
+        if(list1 is not None):
+            
+            lst.append(list1.val)
+            list1=list1.next
+        while(list2 and list2.next is not None):
             lst.append(list2.val)
             list2=list2.next
-        lst.append(list2.val)
-        list2=list2.next
+        if(list2 is not None):
+            
+            lst.append(list2.val)
+            list2=list2.next
         lst.sort()
         finList=dim=ListNode(0)
         for i in range(0,len(lst)):
